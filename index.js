4d86c6b0a8ba9391dfe79ad3cc274bb625eea4e2
mutuajoseph <wambuajoseph756@gmail.com>
2025-03-20 15:34:34 +0300

    add contact section with form and functionality to submit messages

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ let paragraph= 'joseph is a self taught software engineer'
 const skills = ['Problem Solving', 'Good Communicator', 'Team Player', 'Self Taught', 'Fast Learner']
 const projects = ['Project 1', 'Project 2', 'Project 3', 'Project 4', 'Project 5']
 
+
+const submitForm = async (event) => {
+    event.preventDefault()
+    
+    let fullName = document.querySelector('input[type="text"]').value
+    let email = document.querySelector('input[type="email"]').value 
+    let message = document.querySelector('textarea').value
+
+    const data = {
+        fullName,
+        email,
+        message
+    }
+
+   const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        header: {},
+        body: JSON.stringify(data)
+    })
+
+    response.json().then((data) => {
+        console.log(data)
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+
 const aboutMeSection = document.getElementById("aboutme")
 const skillsSection = document.getElementById("skillsprojects")
 
@@ -27,7 +55,15 @@ let skillsList = document.createElement('ul')
 for (let skill = 0; skill < skills.length; skill++ ) {
     console.log(skills[skill])
     let listItem = document.createElement('li')
-    listItem.innerHTML = skills[skill]
+    let listItemText = document.createElement('p')
+    let viewMoreButton = document.createElement('button')
+
+    viewMoreButton.innerHTML = 'View More'
+    listItemText.innerHTML = skills[skill]
+
+    listItem.appendChild(listItemText)
+    listItem.appendChild(viewMoreButton)
+
     skillsList.appendChild(listItem)
 }
 
@@ -42,7 +78,14 @@ let projectList = document.createElement('ol')
 
 projects.forEach((project) => {
     let listItem = document.createElement('li')
+    let listItemText = document.createElement('p')
+    let viewMoreButton = document.createElement('button')
+
+    viewMoreButton.innerHTML = 'View More'
     listItem.innerHTML = project
+
+    listItem.appendChild(listItemText)
+    listItem.appendChild(viewMoreButton)
     projectList.appendChild(listItem)
 })
 
@@ -51,8 +94,46 @@ projectContainer.appendChild(projectList)
 
 // End of skills section 
 
+
+
+// Contact section 
+const contactSection = document.getElementById('contactme')
+
+let contactHeading = document.createElement('h2')
+contactHeading.innerHTML = 'Contact Me'
+
+let contactForm = document.createElement('form')
+
+let fullNameInput = document.createElement('input')
+fullNameInput.setAttribute('type', 'text')
+fullNameInput.setAttribute('placeholder', 'Enter your Full Name')
+
+let emailInput = document.createElement('input')
+emailInput.setAttribute('type', 'email')
+emailInput.setAttribute('placeholder', 'Enter your Email')
+
+let messageInput = document.createElement('textarea')
+messageInput.setAttribute('placeholder', 'Enter your message')
+
+let submitButton = document.createElement('button')
+submitButton.setAttribute('type', 'submit')
+submitButton.innerHTML = 'Submit'
+
+submitButton.addEventListener('click', submitForm)
+
+
+
+contactForm.appendChild(fullNameInput)
+contactForm.appendChild(emailInput)
+contactForm.appendChild(messageInput)
+contactForm.appendChild(submitButton)
+
+contactSection.appendChild(contactHeading)
+contactSection.appendChild(contactForm)
+
+// End of contact section
+
 aboutMeSection.appendChild(headingElement)
 aboutMeSection.appendChild(paragraphElement)
 skillsSection.appendChild(skillsContainer)
 skillsSection.appendChild(projectContainer)
-
